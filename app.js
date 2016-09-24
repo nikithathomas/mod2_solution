99d5af2d66797878479c49004963cede535c553b
nikita <nikita1792@yahoo.com>
2016-09-24 20:43:45 +0530

    Final Version of Files

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 (function () {
 'use strict';
 
-var toBuyList = [
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyShoppingController', ToBuyShoppingController)
+.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyShoppingController(ShoppingListCheckOffService) {
+var toBuy=this;
+toBuy.items=ShoppingListCheckOffService.getToBuyItems();
+toBuy.addItem = function (itemIndex) {
+    ShoppingListCheckOffService.addItem(itemIndex,toBuy.items[itemIndex].name, toBuy.items[itemIndex].quantity);
+  }
+}
+
+
+AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+var alreadyBought= this;
+alreadyBought.items=ShoppingListCheckOffService.getAlreadyBoughtItems();
+}
+
+
+function ShoppingListCheckOffService() {
+  var service = this;
+  var toBuyList = [
   {
-    name: "Milk",
+    name: "Milk Cartons",
     quantity: "2"
   },
   {
@@ -17,21 +41,31 @@ var toBuyList = [
   {
     name: "Chocolate",
     quantity: "5"
+  },
+  {
+    name:"Condensed Milk Tins",
+    quantity:"10"
   }
 ];
 
-var alreadyBoughtlist=[];
+var boughtList = [];
 
-angular.module('ShoppingListCheckOff', [])
-.controller('ToBuyShoppingController', ToBuyShoppingController)
-.controller('AlreadyBoughtShoppingController',AlreadyBoughtShoppingController)
-.service('ShoppingListCheckOffService ', ShoppingListCheckOffService);;
+service.addItem = function (itemIndex,itemName, quantity) {
+    var item = {
+      name: itemName,
+      quantity: quantity
+    };
+    boughtList.push(item);
+    toBuyList.splice(itemIndex, 1);
+  };
 
+  service.getToBuyItems = function () {
+   return toBuyList;
+ };
 
-ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
-function ToBuyShoppingController(ShoppingListCheckOffService) {
-$scope.toBuyList=toBuyList;
-
+ service.getAlreadyBoughtItems=function(){
+   return boughtList;
+ };
 }
 
 })();
